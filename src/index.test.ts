324d25dd2851d3d154dff8fb56b71f2bd14c65a6
Philipp Hagemeister <philipp.hagemeister@boxine.de>
2020-10-13 11:06:36 +0200

    Avoid colors when outputting to a pipe
    kolorist only makes sense for TTYs with color displays. When redirecting the output to a pipe, colors should be suppressed by default.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
 import * as k from './index';
 import * as t from 'assert';
+import * as pty from 'node-pty';
+import * as path from 'path';
+import * as child_process from 'child_process';
+
 
 describe('colors', () => {
+	k.options.enabled = true; // Always enable colors, even in CLI environments
+
 	it('should print colors', () => {
 		t.equal(k.cyan('foo'), '\u001b[36mfoo\u001b[39m');
 	});
@@ -46,6 +52,40 @@ describe('colors', () => {
 		k.options.enabled = true;
 		t.equal(k.cyan('foo'), '\u001b[36mfoo\u001b[39m');
 	});
+});
+
+describe('color switch', () => {
+	it('should be enabled in terminals by default', done => {
+		let output = '';
+		const term = pty.spawn(path.join(__dirname, '..', 'node_modules/.bin/ts-node'), [
+			'-e', 'console.log(require("./index.ts").blue("foo"))'
+		], {
+			name: 'test with pseudo tty',
+			cols: 80,
+			rows: 30,
+			cwd: __dirname,
+		});
+		term.onData(data => output += data);
+		term.onExit(() => {
+			t.equal(JSON.stringify(output.trim()), JSON.stringify('\x1B[34mfoo\x1B[39m'));
+			done();
+		});
+	}).timeout(20000); // typescript is slow
+
+	it('should be disabled in non-interactive terminals', done => {
+		let output = '';
+		const subprocess = child_process.spawn(path.join(__dirname, '..', 'node_modules/.bin/ts-node'), [
+			'-e', 'console.log(require("./index.ts").blue("foo"))'
+		], {
+			cwd: __dirname,
+			stdio: 'pipe',
+		});
+		subprocess.stdout.on('data', data => output += data);
+		subprocess.on('exit', () => {
+			t.equal(JSON.stringify(output.trim()), JSON.stringify('foo'));
+			done();
+		});
+	}).timeout(20000); // typescript is slow
 });
 
 describe('strip colors', () => {
